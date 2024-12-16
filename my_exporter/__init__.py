1fd5fb2b7c509abc071056e81d4b0308b878cb22
Ryota Kobayashi <s.woods.m.29@gmail.com>
2024-12-17 00:04:26 +0900

    Update __init__.py

--- my_exporter/__init__.py
+++ my_exporter/__init__.py
@@ -1,9 +1,10 @@
-# my-exporter/__init__.py
+# my_exporter/__init__.py
 
 from .exporter import export_folder_contents
-from .ignore_handler import load_ignore_patterns
+from .ignore_handler import load_ignore_patterns, load_include_patterns  # Updated
 
 __all__ = [
     'export_folder_contents',
     'load_ignore_patterns',
+    'load_include_patterns',  # Added to __all__
 ]
