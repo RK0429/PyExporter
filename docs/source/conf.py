b0dc791e3a35797ef74513f1f752576631433b82
Ryota Kobayashi <s.woods.m.29@gmail.com>
2024-12-17 01:12:37 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))  # Assuming docs/ is inside the project root
+
 project = 'my-exporter'
 copyright = '2024, Ryota Kobayashi'
 author = 'Ryota Kobayashi'
