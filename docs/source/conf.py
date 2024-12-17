c9be0b0faf018fdb0630435592ef9f6dbaf71745
Ryota Kobayashi <s.woods.m.29@gmail.com>
2024-12-17 11:30:02 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,10 @@ release = '0.3.0'
 
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
     'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
 ]
 
 templates_path = ['_templates']
@@ -30,5 +33,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
